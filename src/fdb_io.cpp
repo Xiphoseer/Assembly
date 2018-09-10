852f1b65782e79821a029142d4f78836018adae4
Daniel Schweighöfer <SchweighoeferD@gmail.com>
2018-09-10 19:52:26 +0200

    Make fdb structs visible in header

--- src/fdb_io.cpp
+++ src/fdb_io.cpp
@@ -9,55 +9,6 @@ namespace assembly::database
 {
     namespace io
     {
-        struct table_header_entry
-        {
-            int32_t column_header_addr;
-            int32_t row_top_header_addr;
-        };
-
-        struct schema_header
-        {
-            int32_t table_count;
-            int32_t table_header_addr;
-        };
-
-        struct column_header
-        {
-            int32_t column_count;
-            int32_t table_name_addr;
-            int32_t column_data_addr;
-        };
-
-        struct column_data
-        {
-            int32_t data_type;
-            int32_t name_addr;
-        };
-
-        struct row_top_header
-        {
-            int32_t slot_count;
-            int32_t row_header_addr;
-        };
-
-        struct row_info
-        {
-            int32_t row_data_header_addr;
-            int32_t next_row_addr;
-        };
-
-        struct row_data_header
-        {
-            int32_t column_count;
-            int32_t row_data_addr;
-        };
-
-        struct row_data
-        {
-            int32_t type;
-            int32_t value;
-        };
-
         void read_int(std::istream& strm, int32_t& val)
         {
             strm.read((char*) &val, 4);
--- src/fdb_io.hpp
+++ src/fdb_io.hpp
@@ -12,6 +12,55 @@
 
 namespace assembly::database::io
 {
+    struct table_header_entry
+    {
+        int32_t column_header_addr;
+        int32_t row_top_header_addr;
+    };
+
+    struct schema_header
+    {
+        int32_t table_count;
+        int32_t table_header_addr;
+    };
+
+    struct column_header
+    {
+        int32_t column_count;
+        int32_t table_name_addr;
+        int32_t column_data_addr;
+    };
+
+    struct column_data
+    {
+        int32_t data_type;
+        int32_t name_addr;
+    };
+
+    struct row_top_header
+    {
+        int32_t slot_count;
+        int32_t row_header_addr;
+    };
+
+    struct row_info
+    {
+        int32_t row_data_header_addr;
+        int32_t next_row_addr;
+    };
+
+    struct row_data_header
+    {
+        int32_t column_count;
+        int32_t row_data_addr;
+    };
+
+    struct row_data
+    {
+        int32_t type;
+        int32_t value;
+    };
+
     //! Read the database from a stream
     int read_from_stream(std::istream& strm, schema& schema, bool data = true);
 
